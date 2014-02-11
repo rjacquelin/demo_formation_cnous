2e6880ae683a37f584b90b9c52a7c82c78cd90ad
Pierre Merlin <pierrem@apido.org>
2014-02-11 15:53:54 +0100

    Update game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,14 @@ require 'spec_helper'
 module Codebraker
   describe Game do
     describe "#start" do
-      it "send a welcome message"
+      it "send a welcome message" do
+        output = double('output')
+        game = Game.new(output)
+        
+        output.should_receive(:puts).with('Welcome to CodeBreaker!')
+        
+        game.start
+      end
     end
   end
 end
